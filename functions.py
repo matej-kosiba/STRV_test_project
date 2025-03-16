f1e15531cf04183ca50cf008c16508720b15b509
Matej <matej.kosiba@gmail.com>
2025-03-16 17:56:04 +0100

    adding new functions for presentation

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import random
 import os
+from sklearn.linear_model import LinearRegression
+import plotly.express as px
 
 def check_year_span(file_path, dataset_name):
     """
@@ -98,7 +101,12 @@ def plot_name_occurance_change_over_time(path_to_NationalNames_dataset, path_to_
     plt.close() 
     
 
-def random_most_unisex_name(path_to_NationalNames_dataset, path_to_StateNames_dataset, dataset, minimum_unisex_score, minimum_name_count, printing_the_names):
+def random_most_unisex_name(path_to_NationalNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\NationalNames.csv", 
+                            path_to_StateNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\StateNames.csv", 
+                            dataset = 'National', 
+                            minimum_unisex_score = 0.9, 
+                            minimum_name_count = 3000, 
+                            printing_the_names = True):
     """
     This function calculates the unisexness of names, the unisex_score, U = min(F, M) / max(F, M), 
     where F is the count of females born with the name and M is the count of males born with the name.
@@ -141,7 +149,30 @@ def random_most_unisex_name(path_to_NationalNames_dataset, path_to_StateNames_da
     random_name = random.choice(unisex_names_sorted.index.tolist())
     return (random_name)
 
-def find_common_national_rare_state(path_to_NationalNames_dataset, path_to_StateNames_dataset, threshold_national=0.7, threshold_state=0.4, sort_by='national', keep_only_the_lowest_state_per_name=False):
+def find_common_national_rare_state(path_to_NationalNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\NationalNames.csv", 
+                                    path_to_StateNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\StateNames.csv", 
+                                    threshold_national = 0.7, 
+                                    threshold_state = 0.4, 
+                                    sort_by = 'National', 
+                                    keep_only_the_lowest_state_per_name = False
+                                    ):
+    """
+    This function finds the most common national names that are also rare at the state level.
+    This is done with thresholding, it is looking for names that are higher in their relative occurance (with respect to the most occuring name) on the national level thaan the 'threshold_national'
+    and at the same time lower in their relative occurance on the state level than the 'threshold_state'.
+
+    Parameters of the function:
+    path_to_NationalNames_dataset - path to the NationalNames dataset
+    path_to_StateNames_dataset - path to the StateNames dataset
+    threshold_national - threshold for filtering names which have relative commonness (*) higher than this value on the national level 
+    threshold_state - threshold to filter names which have relative commonnes on the state level smaller than this value
+    sort_by - either 'National' or 'State', defines by which relative comonness should the output list of names be sorted
+    keep_only_the_lowest_state_per_name - a True or False parameter, if True, it will keep each name in the list only once, with its lowest record in the relative comonness on the state level
+                                                                     if False, it will keep all state entries where the name scored relative comonness on the state level lower than the threshold_state
+
+    (*) relative comonness is defined as a ratio of all name counts with respect to the most occuring name, its value is thus between 1 and 0, where 1 is the most occuring name. 
+    This is calculated for national dataset and stored in the column 'Relative_Commonness_National' and separately also for state dataset per state stored in 'Relative_Commonness_State'
+    """
     
     df_national = pd.read_csv(path_to_NationalNames_dataset)
     df_national_summary = df_national.groupby("Name")[["Count"]].sum().reset_index() # groups dataset by name and sums all counts of each name, converts back to a pandas dataframe
@@ -161,11 +192,130 @@ def find_common_national_rare_state(path_to_NationalNames_dataset, path_to_State
     if keep_only_the_lowest_state_per_name == True: # If we want to monitor each name uniquely, so they do not repeat if low in multiple states and instead keeping only their lowest state
         common_national_rare_state = common_national_rare_state.loc[common_national_rare_state.groupby("Name")["Relative_Commonness_State"].idxmin()]
 
-    if sort_by == 'national': # if we want to sort the output by the "Relative_Commonness_National"
+    if sort_by == 'National': # if we want to sort the output by the "Relative_Commonness_National"
         common_national_rare_state_sorted = common_national_rare_state.sort_values(by='Relative_Commonness_National', ascending=False)
-    elif sort_by == 'state': # if we want to sort the output by the "Relative_Commonness_State"
+    elif sort_by == 'State': # if we want to sort the output by the "Relative_Commonness_State"
         common_national_rare_state_sorted = common_national_rare_state.sort_values(by='Relative_Commonness_State', ascending=False)
     else:
-        raise ValueError("Invalid value. The 'sort_by' expects input to be 'national' or 'state'.")
+        raise ValueError("Invalid value. The 'sort_by' expects input to be 'National' or 'State'.")
     
     print(common_national_rare_state_sorted[['Name', 'Relative_Commonness_National', 'Relative_Commonness_State', 'State']])
+
+def find_trending_names_by_slope(path_to_NationalNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\NationalNames.csv", 
+                                 years_to_analyze=20, 
+                                 plot_top_n = 10, 
+                                 path_to_results = r"C:\Users\matej\VSCode_projects\STRV_test_project\STRV_test_project\Results", 
+                                 plotname_to_save = 'trending_analysis'
+                                 ):
+    """
+    This function fits a linear regression model to measure the slope of change of number of newborns with a certain name over the last 'years_to_analyze' years.
+    It is designed to find and print the top 10 trending names in this time period.
+
+    Parameters:
+    path_to_NationalNames_dataset - path to the NationalNames dataset
+    years_to_analyze - number of years for which we calculate the slope of the trend. It starts from the most recent year.
+    plot_top_n - the number off top trending names to plot, default for the task is 10
+    path_to_results - path for the directory to which the function saves the plots of the 'plot_top_n' names with lin.reg. fit. X axis are the analyzed years, Y axis is the total count of the newborns with the baby name in a year
+    plotname_to_save - starting part of the name of plots for saving, some results of the analysis are added after this starting string 
+    """
+    df_national = pd.read_csv(path_to_NationalNames_dataset)
+    max_year = df_national["Year"].max() # the last year in the dataset
+    min_year = max_year - years_to_analyze + 1  # earliest year to include in analysis
+    recent_years = list(range(min_year, max_year + 1))  # the list of the years to analyze
+
+    df_filtered = df_national[df_national["Year"] >= min_year] # selecting only the last 'years_to_analyze' years
+    df_national = df_filtered.groupby(["Name", "Year"])["Count"].sum().reset_index() # total count of each name in each year across genders
+    recent_counts = df_national[df_national["Year"].isin(recent_years)].groupby("Name")["Count"].sum().reset_index() # calculating total occurance of each name in the entire analyzed period --> good for business needs of startup 
+    recent_counts.rename(columns={"Count": "Recent_Counts"}, inplace=True)
+
+    name_trends = [] # array for storing the steepness of slope after lin.reg. fitting
+
+    for name, group in df_national.groupby("Name"): # iterating through names
+        X = group["Year"].values.reshape(-1, 1)  # X values are  years, reshapen for sklearn
+        y = group["Count"].values # y values are counts
+
+        if len(y) < 2:
+            continue  # lin.reg. needs at least 2 data points for fitting
+
+        model = LinearRegression() 
+        model.fit(X, y) # lin.reg. fitting
+
+        slope = model.coef_.flatten()[0]
+
+        if slope > 0:  # filtering only the positive values of slope, getting rid of highly decreasing names over the insepcted period
+            name_trends.append({"Name": name, "Trend_Slope": slope})
+
+    df_trends = pd.DataFrame(name_trends)
+    df_trends = df_trends.merge(recent_counts, on="Name", how="left")
+    df_trends_sorted = df_trends.sort_values(by="Trend_Slope", ascending=False) # sorting by the steepness 
+    top_trending_names = df_trends_sorted.head(plot_top_n).reset_index(drop=True)
+
+    for i, name in enumerate(top_trending_names["Name"]):
+        name_data = df_national[(df_national["Name"] == name) & (df_national["Year"] >= min_year)] # selecting only the 'years_to_analyze' years for fitting the lin.reg
+        trend_slope = top_trending_names.loc[top_trending_names["Name"] == name, "Trend_Slope"].values[0] # retrieving the slope for a specific name
+
+        X = name_data["Year"].values.reshape(-1, 1)
+        y = name_data["Count"].values
+
+        model = LinearRegression() 
+        model.fit(X, y) # fitting lin.reg.
+
+        X_pred = np.arange(min(X), max(X) + 1).reshape(-1, 1)
+        y_pred = model.predict(X_pred) # predicting the lin.reg. trend
+
+        plt.figure(figsize=(8, 5))
+        plt.scatter(X, y, color="blue", label="Count", alpha=0.6)
+        plt.plot(X_pred, y_pred, color="red", linestyle="-", label="Linear regression", linewidth=1)
+        plt.xticks(np.arange(min(X), max(X) + 1, 1))  # plotting only int. years, not floats
+        plt.xlabel("Year")
+        plt.ylabel("Count")
+        plt.title(f"Trend for {name} over the last {years_to_analyze} years")
+        plt.legend()
+        save_path = os.path.join(path_to_results, f"{plotname_to_save}_for_past_{years_to_analyze}_years_slope_{trend_slope:.0f}_{name}.png")
+        plt.savefig(save_path, dpi=300, bbox_inches="tight")
+        print ('The plot has been saved in the folder Results \n')
+        plt.close() 
+
+    print (top_trending_names)
+
+def top_10_states_most_newborns(path_to_StateNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\StateNames.csv", 
+                                top_N_states = 10
+                                ):
+    """
+    This function loads the StateNames datasets, counts all newborns in each state, sorts states by the number of newborns and prints the top 'top_N_states' states by the number of newborns and the count of newborns.
+
+    Parameters:
+    path_to_StateNames_dataset - path to the StateNames dataset
+    top_N_states - the number of top states we want to print
+    """
+    df = pd.read_csv(path_to_StateNames_dataset)
+    total_per_state = df.groupby("State")["Count"].sum().reset_index() # number of all newborns per state
+    top_states = total_per_state.sort_values(by="Count", ascending=False).head(top_N_states).reset_index(drop=True) # sorts states by the number of all newborns, keeps the top 'top_N_states' and resets the original indexes by the new order
+    
+    print(top_states)
+
+def plot_top_names_by_state(path_to_StateNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\StateNames.csv"):
+    """
+    This function craetes a choropleth map of the USA where each state is colored by the most popular baby name in that state.
+
+    Parameters:
+    file_path - path to the StateNames dataset
+    """
+    df = pd.read_csv(path_to_StateNames_dataset)
+    top_names = df.groupby(["State", "Name"])["Count"].sum().reset_index() #sums all occurances of the same baby name in the same state across all years,  .groupby converts the data frame to a grouped object, .reset_index converts it back
+    top_names = top_names.loc[top_names.groupby("State")["Count"].idxmax()]  # creates a grouped object - grouping states together, .idmax finds the index of the maximum count (done for each state), finally .loc makes a subsample of rows corresponding to the most occuring baby names per state
+    
+    # Create map visualization
+    fig = px.choropleth(
+        top_names,
+        locations="State",
+        locationmode="USA-states",
+        color="Name", # coloring states by the most popular name
+        hover_name="Name", # displays the most popular baby name of the state when hovering over it with a coursor
+        hover_data={"State": True, "Name": True, "Count": True}, # additional data shown when hovering over
+        title="Most Popular Baby Names by State",
+        scope="usa", # defines the map to be only USA
+        color_discrete_sequence=px.colors.qualitative.Set3
+    )
+    
+    fig.show()
--- main.py
+++ main.py
@@ -12,11 +12,13 @@ import functions
 path_to_results = r"C:\Users\matej\VSCode_projects\STRV_test_project\STRV_test_project\Results"
 path_to_NationalNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\NationalNames.csv"
 path_to_StateNames_dataset = r"C:\Users\matej\.cache\kagglehub\datasets\kaggle\us-baby-names\versions\2\StateNames.csv"
-
+print ()
+print (' -----------------------------------------------------')
 print ('Exploring the fundamentals of the datasets')
 functions.check_year_span(path_to_NationalNames_dataset, 'NationalNames')
 functions.check_year_span(path_to_StateNames_dataset, 'StateNames')
-
+print ()
+print (' -----------------------------------------------------')
 print ('Explore the dataset, task 1. How did the name Ida change period-over-period nationally?')
 functions.plot_name_occurance_change_over_time(path_to_NationalNames_dataset = path_to_NationalNames_dataset, 
                                                path_to_StateNames_dataset = path_to_StateNames_dataset, 
@@ -32,7 +34,8 @@ functions.average_occurrence_in_time_period_national(path_to_NationalNames_datas
                                                      start_year = 1910, 
                                                      end_year = 1930
                                                      ) # just to get some statistics for the documentation
-
+print ()
+print (' -----------------------------------------------------')
 print ('Explore the dataset, task 2. How did the name Ida change period-over-period in California?')
 functions.plot_name_occurance_change_over_time(path_to_NationalNames_dataset = path_to_NationalNames_dataset, 
                                                path_to_StateNames_dataset = path_to_StateNames_dataset, 
@@ -42,7 +45,8 @@ functions.plot_name_occurance_change_over_time(path_to_NationalNames_dataset = p
                                                path_to_results = path_to_results, 
                                                plotname_to_save = 'Ida_change_over_time_state_CA'
                                                )
-
+print ()
+print (' -----------------------------------------------------')
 print ('Explore the dataset, task 3. What name is the most unisex?')
 functions.random_most_unisex_name(path_to_NationalNames_dataset = path_to_NationalNames_dataset, 
                                   path_to_StateNames_dataset = path_to_StateNames_dataset,
@@ -51,13 +55,33 @@ functions.random_most_unisex_name(path_to_NationalNames_dataset = path_to_Nation
                                   minimum_name_count = 10000, 
                                   printing_the_names = False
                                   )
-
+print ()
+print (' -----------------------------------------------------')
 print ('Explore the dataset, task 4. Which names are common nationally but rare at the state level?')
 functions.find_common_national_rare_state(path_to_NationalNames_dataset = path_to_NationalNames_dataset, 
                                           path_to_StateNames_dataset = path_to_StateNames_dataset,
                                           threshold_national=0.75, 
                                           threshold_state=0.65, 
-                                          sort_by='state', 
+                                          sort_by='State', 
                                           keep_only_the_lowest_state_per_name=True
                                           )
+print ()
+print (' -----------------------------------------------------')
+print ('Presentation task 1. Find the TOP 10 trending names')
 
+functions.find_trending_names_by_slope(path_to_NationalNames_dataset = path_to_NationalNames_dataset,
+                                       years_to_analyze=5, 
+                                       plot_top_n = 10,
+                                       path_to_results = path_to_results,
+                                       plotname_to_save = 'trending_analysis'
+                                       )
+print ()
+print (' -----------------------------------------------------')
+print ('Presentation task 2. Find the TOP 10 states with the most newborns')
+functions.top_10_states_most_newborns(path_to_StateNames_dataset = path_to_StateNames_dataset, 
+                                      top_N_states = 10
+                                      )
+print ()
+print (' -----------------------------------------------------')
+print ('Presentation task 3. Make a map of the USA showing the top names by state')
+functions.plot_top_names_by_state(path_to_StateNames_dataset)
